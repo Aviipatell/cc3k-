62ff7a053c12cf4b8df720ee7f181f8f196bee55
Dhruv Mathur <dhruv.mathur03@gmail.com>
2023-04-03 13:22:23 -0400

    added files to item folder

---
+++ Items/item.h
@@ -0,0 +1,11 @@
+#ifndef __ITEM_H_
+#define __ITEM_H_
+
+class Entity;
+
+class Item: public Entity {
+public:
+    virtual void useItem() = 0;
+};
+
+#endif
---
+++ Items/protected_item.h
@@ -0,0 +1,15 @@
+#ifndef __PROTECTED_ITEM_H_
+#define __PROTECTED_ITEM_H_
+
+#include "item.h"
+
+class Dragon;
+
+class ProtectedItem: public Item {
+    Dragon *dragon;
+public:
+    void setDragon(Dragon *d);
+    void destroyDragon();
+};
+
+#endif
