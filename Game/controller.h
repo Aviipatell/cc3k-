97f41f7af01ddc953eef804f71ebeb24e1ec2e1d
Dhruv Mathur <dhruv.mathur03@gmail.com>
2023-04-03 14:18:35 -0400

    added interface files for game, controller and td

---
+++ Game/controller.h
@@ -0,0 +1,13 @@
+#ifndef __CONTROLLER_H_
+#define __CONTROLLER_H_
+
+class Game;
+
+class Controller {
+    Game *g;
+public:
+    void startGame(char race);
+    void restartGame(char race);
+    void quitGame();
+    void print();
+};
---
+++ Game/text_display.h
@@ -0,0 +1,10 @@
+#ifndef __TEXT_DISPLAY_H_
+#define __TEXT_DISPLAY_H_
+
+#include <vector>
+
+class TextDisplay {
+    std::vector<std::vector<char>> theGrid;
+public:
+    void printGrid();
+};
